c690065f21a50ab19a4df35b4488ed478fd6df13
qingfuli <184907661@qq.com>
2018-07-16 20:21:42 +0800

    mybatis 配置

--- src/main/java/shopadmin/config/AppConfig.java
+++ src/main/java/shopadmin/config/AppConfig.java
@@ -1,19 +1,55 @@
 package shopadmin.config;
 
+import javax.sql.DataSource;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@ComponentScan("shop")
+@ComponentScan("shopadmin")
 @EnableWebMvc
+@MapperScan("admin.mapper")
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig extends WebMvcConfigurerAdapter{
 	
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/jsp/",".jsp");
 	}
-
+	
+	@Bean
+	public DataSource dataSource(Environment env) { 
+		DriverManagerDataSource ds = new DriverManagerDataSource(
+				env.getProperty("jdbc.url"),
+				env.getProperty("jdbc.username"),
+				env.getProperty("jdbc.password"));
+		ds.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		return ds;
+	}	
+	
+	@Bean 
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) {
+		SqlSessionFactoryBean sf = new SqlSessionFactoryBean();
+		sf.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+		sf.setDataSource(dataSource);
+		return sf;
+	}	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+	}
+	
 }
